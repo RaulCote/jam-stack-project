de3e1dab8534bc88780be7e3182dacd75383aee6
RaulCote <raulcote@hotmail.com>
2019-03-09 13:05:35 +0100

    Footer responsive.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,65 +9,71 @@ import instagram from '../img/social/instagram.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
+      <footer className="footer">
+
+        <div className="footer-logo-container">
           <img
             src={logo}
-            alt="Kaldi"
-            style={{ width: '12em', height: '10em' }}
+            alt="Ika Editions"
+            style={{ width: '9em', height: '7em' }}
           />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-              <div className="columns">
-                <div className="column is-4">
-                <section className="menu">
-                    <ul className="menu-list">
-                      <li><Link to="/" className="navbar-item">Home</Link></li>
-                      <li><Link className="navbar-item" to="/about">About</Link></li>
-                      <li><Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                    <Link className="navbar-item" to="/contact/examples">
-                      Form Examples
-                    </Link>
-                    </li>
-                    <li><a
-                      className="navbar-item"
-                      href="/admin/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Admin
-                    </a></li>
+
+        <div className="footer-info">
+
+          {/* <div className="footer-container"> */}
+
+              <div className="footer-columns">
+
+                <div className="footer-column">
+                  <section className="menu">
+                      <ul className="footer-links">
+                        <li><Link activeStyle={{ color: 'white' }} to="/" className="navbar-item">Home</Link></li>
+                        <li><Link activeStyle={{ color: 'white' }} className="navbar-item" to="/about">About</Link></li>
+                        <li><Link activeStyle={{ color: 'white' }} className="navbar-item" to="/products">Products</Link></li>
+                        <li><Link activeStyle={{ color: 'white' }} className="navbar-item" to="/contact/examples">Form Examples</Link></li>
+                      {/* <li><a
+                                className="navbar-item"
+                                href="/admin/"
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                  Admin
+                              </a>
+                          </li> */}
+                      </ul>
+                  </section>
+                </div>
+
+                <div className="footer-column">
+                  <section>
+                    <ul className="footer-links">
+                      <li>
+                        <Link className="navbar-item" to="/blog">
+                          Latest Stories
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="navbar-item" to="/contact">
+                          Contact
+                        </Link>
+                      </li>
                     </ul>
                   </section>
                 </div>
-                <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                  <li>
-                    <Link className="navbar-item" to="/blog">
-                      Latest Stories
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="navbar-item" to="/contact">
-                      Contact
-                    </Link>
-                  </li>
-                  </ul>
-                </section>
-                </div>
-                <div className="column is-4 social">
-                
+
+                <div className="footer-column social">
                   <a title="facebook" href="https://facebook.com/ikaeditions">
                     <img 
                       src={facebook}
                       alt="Facebook"
                       style={{ width: '1em', height: '1em' }}
+                    />
+                  </a>
+                  <a title="instagram" href="https://instagram.com/ika_editions">
+                    <img
+                    src={instagram}
+                    alt="Instagram"
+                    style={{ width: '1em', height: '1em' }}
                     />
                   </a>
                   {/* <a title="twitter" href="https://twitter.com">
@@ -78,13 +84,6 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a> */}
-                <a title="instagram" href="https://instagram.com/ika_editions">
-                  <img
-                  src={instagram}
-                  alt="Instagram"
-                  style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
                 {/* <a title="vimeo" href="https://vimeo.com">
                   <img
                   src={vimeo}
@@ -93,9 +92,13 @@ const Footer = class extends React.Component {
                   />
                 </a> */}
                 </div>
-              </div>
+
+              {/* </div> */}
+
             </div>
+
         </div>
+
       </footer>
     )
   }
