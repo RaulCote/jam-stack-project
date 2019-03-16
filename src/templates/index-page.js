3838d13123c934622c92a24d367f97e960c15df7
RaulCote <raulcote@hotmail.com>
2019-03-16 11:10:35 +0100

    Some Landing Page css.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -18,7 +18,7 @@ export const IndexPageTemplate = ({
 }) => (
     <div>
         <div
-          className="full-width-image margin-top-0"
+          className="landing-image margin-top-0"
           style={{
             backgroundImage: `url(${
               !!image.childImageSharp
@@ -27,40 +27,33 @@ export const IndexPageTemplate = ({
             })`,
             backgroundPosition: `top left`,
             backgroundAttachment: `fixed`,
-          }}
-    >
-  <div style={{
-        display: 'flex',
-        height: '150px',
-        lineHeight: '1',
-        justifyContent: 'space-around',
-        alignItems: 'left',
-        flexDirection: 'column' }}>
-      <h1
-        className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-        style={{
-          boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-          backgroundColor: 'rgb(255, 68, 0)',
-          color: 'white',
-          lineHeight: '1',
-          padding: '0.25em'
-        }}
-      >
-        {title}
-      </h1>
-      <h3 className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em'
-          }}
-      >
-        {subheading}
-      </h3>
-      </div>
-    </div>
+          }}>
+          <div className="landing-main-text">
+              <h1
+                className="nada"
+                style={{
+                  boxShadow: 'rgba(17, 17, 17, 1) 0.5rem 0px 0px, rgba(17, 17, 17, 1) -0.5rem 0px 0px',
+                  backgroundColor: 'rgba(17, 17, 17, 1)',
+                  color: 'white',
+                  lineHeight: '1',
+                  padding: '0.25em'
+                }}
+              >
+              {title}
+              </h1>
+              <h3 className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+                  style={{
+                    boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
+                    backgroundColor: 'rgb(255, 68, 0)',
+                    color: 'white',
+                    lineHeight: '1',
+                    padding: '0.25em'
+                  }}
+              >
+              {subheading}
+              </h3>
+          </div>
+        </div>
   <section className="section section--gradient">
     <div className="container">
       <div className="section">
