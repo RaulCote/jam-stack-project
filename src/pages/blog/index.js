a550b22aa29bb5abef36eb08f45dbdb574b3a596
RaulCote <raulcote@hotmail.com>
2019-04-02 21:11:01 +0200

    Commit.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -14,14 +14,14 @@ export default class BlogIndexPage extends React.Component {
             <div
                 className="full-width-image-container margin-top-0"
                 style={{
-                  backgroundImage: `url('/img/blog-index.jpg')`,
+                  backgroundImage: `url('/img/latest-stories.jpg')`,
                 }}
               >
                 <h1
                   className="has-text-weight-bold is-size-1"
                   style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
+                    boxShadow: '0.5rem 0 0 rgba(17, 17, 17, 1), -0.5rem 0 0 rgba(17, 17, 17, 1)',
+                    backgroundColor: 'rgba(17, 17, 17, 1)',
                     color: 'white',
                     padding: '1rem',
                   }}
