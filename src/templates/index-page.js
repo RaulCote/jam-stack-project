6c43ba7b6fd1dca944d58a28216b51ab1ae83cfd
RaulCote <raulcote@hotmail.com>
2019-04-04 21:01:56 +0200

    Landing text aligned correctly.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -18,7 +18,7 @@ export const IndexPageTemplate = ({
 }) => (
     <div>
         <div
-          className="full-width-image-container margin-top-0"
+          className="landing-image margin-top-0"
           style={{
             backgroundImage: `url(${
               !!image.childImageSharp
