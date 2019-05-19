da6c162c863a8bfa7b48e9ad69e5fd6e9c016d01
RaulCote <raulcote@hotmail.com>
2019-05-19 17:35:54 +0200

    Routes for "Shop" and "Videos" created.

---
+++ src/templates/videos-page.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+
+export const VideosPageTemplate = ({
+    image,
+    title,
+    heading,
+    description,
+    intro,
+    main,
+    testimonials,
+    fullImage,
+    pricing,
+  }) => (
+    <section className="section section--gradient">
+       
+    </section>
+  )
+  
+  VideosPageTemplate.propTypes = {
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    title: PropTypes.string,
+    heading: PropTypes.string,
+    description: PropTypes.string,
+    intro: PropTypes.shape({
+      blurbs: PropTypes.array,
+    }),
+    main: PropTypes.shape({
+      heading: PropTypes.string,
+      description: PropTypes.string,
+      image1: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+      image2: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+      image3: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    }),
+    testimonials: PropTypes.array,
+    fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    pricing: PropTypes.shape({
+      heading: PropTypes.string,
+      description: PropTypes.string,
+      plans: PropTypes.array,
+    }),
+  }
+  
+  const VideosPage = ({ data }) => {
+    const { frontmatter } = data.markdownRemark
+  
+    return (
+      <Layout>
+        <VideosPageTemplate
+          image={frontmatter.image}
+          title={frontmatter.title}
+          heading={frontmatter.heading}
+          description={frontmatter.description}
+          intro={frontmatter.intro}
+          main={frontmatter.main}
+          testimonials={frontmatter.testimonials}
+          fullImage={frontmatter.full_image}
+          pricing={frontmatter.pricing}
+        />
+      </Layout>
+    )
+  }
+  
+  VideosPage.propTypes = {
+    data: PropTypes.shape({
+      markdownRemark: PropTypes.shape({
+        frontmatter: PropTypes.object,
+      }),
+    }),
+  }
+  
+  export default VideosPage
+  
+  export const VideosPageQuery = graphql`
+    query VideosPage($id: String!) {
+      markdownRemark(id: { eq: $id }) {
+        frontmatter {
+          title
+          image {
+            childImageSharp {
+              fluid(maxWidth: 2048, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          heading
+          description
+          intro {
+            blurbs {
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 240, quality: 64) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+              text
+            }
+            heading
+            description
+          }
+          main {
+            heading
+            description
+            image1 {
+              alt
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 526, quality: 92) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+            image2 {
+              alt
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 526, quality: 92) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+            image3 {
+              alt
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 1075, quality: 72) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+          testimonials {
+            author
+            quote
+          }
+          full_image {
+            childImageSharp {
+              fluid(maxWidth: 2048, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          pricing {
+            heading
+            description
+            plans {
+              description
+              items
+              plan
+              price
+            }
+          }
+        }
+      }
+    }
+  `
