53522ef2314cbd2de6cce5115ed0c43af767c2f0
RaulCote <raulcote@hotmail.com>
2019-05-26 16:22:45 +0200

    Cleaning comments Iframe Component.

--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -1,22 +1,5 @@
 import React from 'react';
 
-// const IframeComponent = () => (
-//     <iframe
-//       title="snapwidget-iframe"
-//       allowtransparency="true" 
-//       className="snapwidget-ika" 
-//       frameBorder="0" 
-//       scrolling="no" 
-//       src="https://snapwidget.com/embed/293837" 
-//       style={{
-//         border: 'none', 
-//          overflow: 'auto', 
-//          height: '100%'
-//       }}
-//       >
-//     </iframe>
-// )
-
 class IframeComponent extends React.Component {
   
   componentDidMount() {
