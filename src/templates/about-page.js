590215dde76e7b0e9f9ec780045f2b433efadb62
RaulCote <raulcote@hotmail.com>
2019-05-26 20:00:02 +0200

    About margins corrected.

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -13,27 +13,17 @@ export const AboutPageTemplate = ({ title, image, content, contentComponent }) =
           <div className="columns">
             <div className="column is-10 is-offset-1">
               <div className="section">
-                {/* <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                  {title}
-                </h2> */}
                 <div
                   className="landing-image margin-top-0"
                   style={{
-                    // backgroundImage: `url(${landingImage})`,
-                    // backgroundPosition: '10%'
                     backgroundImage: `url(${
                       !!image.childImageSharp
                         ? image.childImageSharp.fluid.src
                         : image
                     })`,
-                    backgroundPosition: `10%`,
-                    // marginBottom: '60px'
+                    backgroundPosition: `10%`
                   }} />
-                <h4 
-                  className={'section-class'}
-                  style={{
-                    marginBottom: '100px'
-                  }}>
+                <h4 className={'section-class'}>
                   {title}
                 </h4>
                 <PageContent className="about-container" content={content} />
@@ -43,8 +33,7 @@ export const AboutPageTemplate = ({ title, image, content, contentComponent }) =
                   className={'google-maps-iframe'}
                   title="google-maps-iframe"
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2992.899568772054!2d2.156959215197784!3d41.39798737926315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a296855fe531%3A0xa2e24fe3d1c282cf!2sIKA!5e0!3m2!1ses!2ses!4v1555941780833!5m2!1ses!2ses" 
-                  style={{border: '0'}} 
-                  />
+                  style={{border: '0'}} />
                 </div>
             </div>
           </div>
