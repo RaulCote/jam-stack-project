174e0801312477539280fdb0952c37243e28c303
RaulCote <raulcote@hotmail.com>
2019-05-26 18:02:36 +0200

    About page dynamic Tab title.

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,35 +1,35 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const AboutPageTemplate = ({ title, image, content, contentComponent }) => {
   const PageContent = contentComponent || Content
-
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              {/* <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2> */}
-              <PageContent className="container-text" content={content} />
-            </div>
-            <div className="google-maps-container">
-              <iframe 
-                className={'google-maps-iframe'}
-                title="google-maps-iframe"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2992.899568772054!2d2.156959215197784!3d41.39798737926315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a296855fe531%3A0xa2e24fe3d1c282cf!2sIKA!5e0!3m2!1ses!2ses!4v1555941780833!5m2!1ses!2ses" 
-                style={{border: '0'}} 
-                 />
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+              <div className="section">
+                {/* <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                  {title}
+                </h2> */}
+                <PageContent className="container-text" content={content} />
               </div>
+              <div className="google-maps-container">
+                <iframe 
+                  className={'google-maps-iframe'}
+                  title="google-maps-iframe"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2992.899568772054!2d2.156959215197784!3d41.39798737926315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a296855fe531%3A0xa2e24fe3d1c282cf!2sIKA!5e0!3m2!1ses!2ses!4v1555941780833!5m2!1ses!2ses" 
+                  style={{border: '0'}} 
+                  />
+                </div>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   )
 }
 
@@ -41,13 +41,16 @@ AboutPageTemplate.propTypes = {
 
 const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data
+  const title = data.site.siteMetadata.title
 
   return (
     <Layout>
+      <Helmet title={`${title} | ${post.frontmatter.title}`} />
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        image={post.frontmatter.image}
       />
     </Layout>
   )
@@ -61,10 +64,22 @@ export default AboutPage
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
         title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
