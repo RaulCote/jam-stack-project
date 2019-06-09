f0818742481ded0a11ca6c4854ff7689a4bced05
RaulCote <raulcote@hotmail.com>
2019-06-09 13:39:25 +0200

    Burguer menu on the way.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,21 @@ import { Link } from 'gatsby'
 import logo from '../img/ika-logo.png'
 
 const Navbar = class extends React.Component {
+
   
+  toggleNavBar = () => {
+    const navMenu = document.getElementById('navMenu');
+    const body = document.querySelector('body');
+
+    if (navMenu.className === 'navbar-menu') {
+      navMenu.className += ' responsive';
+      body.setAttribute('style', 'overflow-y: hidden') 
+    } else {
+      navMenu.className = 'navbar-menu';
+      body.setAttribute('style', 'overflow-y: scroll')
+    }
+  }
+
   render() {
     return (
       <nav
@@ -12,14 +26,17 @@ const Navbar = class extends React.Component {
         aria-label="main-navigation">
 
         <div className="navbar-container">
-          <div className="navbar-brand">
+          {/* <div className="navbar-brand">
             <Link to="/" className="logo-navbar-container" title="Logo">
               <img src={logo} alt="Ika" className="logo-navbar" />
             </Link>
-          </div>
+          </div> */}
 
           <div id="navMenu" className="navbar-menu">
             <div className="navbar-start">
+              <Link to="/" className="logo-navbar-container" title="Logo">
+                <img src={logo} alt="Ika" className="logo-navbar" />
+              </Link>
               <Link 
                 className='navbar-item'
                 to="/about" 
@@ -50,6 +67,15 @@ const Navbar = class extends React.Component {
                 activeStyle={{ color: 'white' }}>
                 News
               </Link>
+              <a 
+                href="javascript:void(0);" 
+                className="icon navbar-item burguer" 
+                onClick={() => this.toggleNavBar()}>
+                {/* <i>X</i> */}
+                <div className={'burguer-bar'} />
+                {/* <div className={'burguer-bar'} /> */}
+                <div className={'burguer-bar'} />
+              </a>
             </div>
           </div>
           
